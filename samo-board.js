a1d5ac87100b34b59b5b0fe81ba018a46ab715c1
liangqj3 <liangqj3@midea.com>
2021-09-04 17:02:44 +0800

    临时提交

--- samo-board.js
+++ samo-board.js
@@ -1,6 +1,8 @@
 "use strict";
 import cloneDeep from './lodash.clonedeep.js'
 export default class samoBoard {
+  customRender = undefined;
+  #wheelFn = undefined;
   #wrap;
   #ctx;
   #canvas;
@@ -8,12 +10,17 @@ export default class samoBoard {
   #zoomSize = 1;
   #oldZoomSize = 1;
   #bgList = [];
+  #spacebarPressed = false;
+  #clickTimeLogs=[];
   #draws = [];
   #drawType = 'pointer';
   #pencilDownFn = undefined;
   #pencilMoveFn = undefined;
   #pencilUpFn = undefined;
   #pencilOutFn = undefined;
+  #maxZoomSize=1.5;
+  #minZoomSize=0.5;
+  #zoomStep = 0.05;
   #svgCursors = {
     location: {
       path: 'M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12 ,2Z',
@@ -26,32 +33,36 @@ export default class samoBoard {
     x: 0,
     y: 0
   }
+  #drawPrototype = () => {
+    return {
+      id: this.constructor.uuidv4Short(),
+      x: 0, // polygon, rect, arc, arcto use
+      y: 0, // polygon, rect, arc, arcto use
+      text: '', // text use
+      lock: false, 
+      width: 0, // rect use
+      height: 0, // rect use
+      radius: 0, // arc, arcto use
+      startAngle: 0, // arc use
+      endAngle: 0, // arc use
+      selected: false,
+      ways:[ // polygon, arcto use
+        // {
+        //   x:0,
+        //   y:0,
+        // }
+      ],
+      type: 'rect', // polygon, arc, arcto, text
+      drawType: 'rect', // 用户自定义
+      rotate: 0,
+      translate: [],
+      gco: 'source-cover',
+      strokeStyle: '#333',
+      fillStyle: 'translate',
+      lineWidth: 1
+    }
+  }
   #tmpDraw = {};
-  #drawPrototype = {
-    x: 0, // polygon, rect, arc, arcto use
-    y: 0, // polygon, rect, arc, arcto use
-    text: '', // text use
-    lock: false, 
-    width: 0, // rect use
-    height: 0, // rect use
-    radius: 0, // arc, arcto use
-    startAngle: 0, // arc use
-    endAngle: 0, // arc use
-    ways:[ // polygon, arcto use
-      // {
-      //   x:0,
-      //   y:0,
-      // }
-    ],
-    type: 'rect', // polygon, arc, arcto, text
-    drawType: 'rect', // 用户自定义
-    rotate: 0,
-    translate: [],
-    gco: 'source-cover',
-    strokeStyle: '#333',
-    fillStyle: 'gray',
-    lineWidth: 3
-  };
   constructor(options={}) {
     this.opts = this.constructor.mergeDeep({
       wrap: undefined,
@@ -59,7 +70,7 @@ export default class samoBoard {
       ctxStyle: {
         strokeStyle: '#333',
         fillStyle: 'transparent',
-        lineWidth: 3, 
+        lineWidth: 1, 
         lineJoin: 'miter', // bevel, round, miter;
         lineCap: 'butt', // butt, round, square
         gco: 'source-cover'
@@ -86,10 +97,169 @@ export default class samoBoard {
     }
     // this.#canvas.style.cssText = `display:grid;background-color: #fff;`
     this.#setCtx(this.#ctx, this.opts.ctxStyle)
-    this.renderBoard()
 
+    this.#wheelFn = e => this.#canvasWheel(e)
+    this.#canvas.oncontextmenu = e => {
+      e.preventDefault();
+    };
+    this.#canvas.addEventListener('wheel', this.#wheelFn, false);
+    this.#bindKeysEvents()
+
+    this.#draws = new Proxy(this.#draws, {
+      get: function(target, propKey, receiver){
+        console.log(`getting ${target}!`);
+        
+      },
+      set: function(target, propKey, receiver) {
+        console.log(target)
+      }
+    })
+
+    this.renderBoard()
     this.setDrawType()
     return this;
+  }
+  #bindKeysEvents() {
+    window.addEventListener('keydown', (e)=>this.#windowKeyDown(e), false);
+    window.addEventListener('keyup', (e)=>this.#windowKeyUp(e), false);
+  }
+  #revokeKeysEvents() {
+    window.removeEventListener('keydown', this.#windowKeyDown, false);
+    window.removeEventListener('keyup', this.#windowKeyUp, false);
+  }
+  #windowKeyDown(e) {
+    // console.log(e.keyCode)
+    const _keyCode = e.keyCode
+    switch(_keyCode) {
+      case 32:
+        if (!this.#spacebarPressed) {
+          document.documentElement.style.cursor = 'grab'
+          this.#spacebarPressed = true;
+        }
+        break;
+    }
+  }
+  #windowKeyUp(e) {
+    // console.log(e.keyCode)
+    const _keyCode = e.keyCode
+    switch(_keyCode) {
+      case 32:
+        if (this.#spacebarPressed) {
+          document.documentElement.style.cursor = 'default'
+          this.#spacebarPressed = false;
+        }
+        break;
+    }
+  }
+  #setCtx(ctx, configs={}){
+    if (!ctx) {
+      return;
+    }
+    for (let key in configs) {
+      if (key === 'gco') {
+        ctx.globalCompositeOperation = configs[key];
+      } else {
+        ctx[key] = configs[key];
+      }
+    }
+  }
+  #drawToSvgPath(draw, origin=false) {
+    try {
+      let _svg_path = [`M${draw.x} ${draw.y}`];
+      switch(draw.type) {
+        case "rect":
+          _svg_path.push(`L${draw.x+draw.width} ${draw.y}`);
+          _svg_path.push(`L${draw.x+draw.width} ${draw.y+draw.height}`);
+          _svg_path.push(`L${draw.x} ${draw.y+draw.height}`);
+          break;
+        case "polygon":
+          draw.ways.forEach(val => {
+            _svg_path.push(`L${val.x} ${val.y}`);
+          });
+          break;
+      }
+      _svg_path.push('Z');
+      _svg_path = _svg_path.join(' ');
+      if (origin) {
+        return _svg_path
+      } else {
+        return new Path2D(_svg_path);
+      }
+    } catch(err) {
+      throw err
+    }
+  }
+  // 产生短id
+  static uuidv4Short() {
+    return 'xxxx-4xxxyx'.replace(/[xy]/g, function(c) {
+      const r = (Math.random() * 16) | 0,
+        v = c == 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
+  // 产生id
+  static generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      let r = (Math.random() * 16) | 0,
+        v = c == 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
+  static cloneDeep(params) {
+    return cloneDeep(params)
+  }
+  static mergeDeep(...objects) {
+    const isObject = (obj) => obj && obj.constructor === Object;
+    return objects.reduce((prev, obj) => {
+      Object.keys(obj).forEach((key) => {
+        const pVal = prev[key];
+        const oVal = obj[key];
+        if (Array.isArray(pVal) && Array.isArray(oVal)) {
+          prev[key] = pVal.concat(...oVal);
+        } else if (isObject(pVal) && isObject(oVal)) {
+          prev[key] = this.mergeDeep(pVal, oVal);
+        } else {
+          prev[key] = oVal;
+        }
+      });
+      return prev;
+    }, {});
+  }
+  selectDraw({useStroke=false, pointX=0, pointY=0, single=false}) {
+    if (this.#draws && this.#draws.length) {
+      if (single) {
+        this.#draws.forEach(val => val.selected = false)
+      }
+      for (let i=(this.#draws.length-1);i>=0;i--) {
+        let val = this.#draws[i]
+        const _path = this.#drawToSvgPath(val)
+        let _flag = false;
+        if (useStroke) {
+          _flag = this.#ctx.isPointInStroke(_path, pointX, pointY)
+        } else {
+          _flag = this.#ctx.isPointInPath(_path, pointX, pointY)
+        }
+        if (_flag) {
+          val.selected = true;
+          break;
+        }
+      }
+    }
+  }
+  exportDrawsData() {
+    this.#draws.forEach(val => {
+      val['x'] = Math.round(val.x)
+      val['y'] = Math.round(val.y)
+      val['width'] = Math.round(val.width)
+      val['height'] = Math.round(val.height)
+      if (val.ways && val.ways.length) {
+        val.ways.forEach(wal=>{
+          wal['x'] = Math.round(wal.x)
+          wal['y'] = Math.round(wal.y)
+        })
+      }
+    })
+    return this.constructor.cloneDeep(this.#draws)
   }
   getCanvas() {
     return this.#canvas
@@ -113,91 +283,13 @@ export default class samoBoard {
     }
     return this.#canvas.style.cssText = _style.join(';');
   }
-  static cloneDeep(params) {
-    return cloneDeep(params)
-  }
-  static mergeDeep(...objects) {
-    const isObject = (obj) => obj && obj.constructor === Object;
-    return objects.reduce((prev, obj) => {
-      Object.keys(obj).forEach((key) => {
-        const pVal = prev[key];
-        const oVal = obj[key];
-        if (Array.isArray(pVal) && Array.isArray(oVal)) {
-          prev[key] = pVal.concat(...oVal);
-        } else if (isObject(pVal) && isObject(oVal)) {
-          prev[key] = this.mergeDeep(pVal, oVal);
-        } else {
-          prev[key] = oVal;
-        }
-      });
-      return prev;
-    }, {});
-  }
-  #setCtx(ctx, configs={}){
-    if (!ctx) {
-      return;
-    }
-    for (let key in configs) {
-      if (key === 'gco') {
-        ctx.globalCompositeOperation = configs[key];
-      } else {
-        ctx[key] = configs[key];
-      }
-    }
-  }
-  #drawToSvgPath(polygon, svgout=false) {
-    let _svg_path = [`M${polygon.x} ${polygon.y}`];
-    polygon.ways.forEach(val => {
-      _svg_path.push(`L${val.x} ${val.y}`);
-    });
-    _svg_path.push('Z');
-    _svg_path = _svg_path.join(' ');
-    // console.log(`_svg_path: ${_svg_path}`)
-    if (svgout) {
-      return _svg_path
-    } else {
-      return new Path2D(_svg_path);
-    }
-  }
-  // 计算缩放后拖拉后的差值
-  #calcZoomedDragoffsetDeltaSize(zoomin = true) {
-    
-    return this.#dragOffset;
-  }
-  // 计算当前缩放尺寸
-  #calcCurrentZoomSize(size, plus = true, step = 0.01, min = 0.15, max = 2) {
-    if (isNaN(size)) {
-      console.warn('size param is not a number');
-      return null;
-    }
-    this.#oldZoomSize = size;
-    size = plus ? size + step : size - step;
-    let _originScaled = 1;
-    // if (this.bgObj.scaled) {
-    //   _originScaled = this.bgObj.scaled
-    // }
-    // if (this.spliceBgList && this.spliceBgList.length) {
-    //   _originScaled = this.spliceBgList[this.spliceBgList.length-1].scaled
-    // }
-    const _min = Math.min(_originScaled, 1);
-    return Math.max(_min, Math.min(parseFloat(size.toFixed(3)), max));
-  }
   // 还原缩放
   zoomReset() {
-    // this.calcZoomedDragoffsetDeltaSize(false);
-    if (this.bgObj && this.bgObj.data) {
-      this.#dragOffset = {
-        x: this.bgObj.offsetX,
-        y: this.bgObj.offsetY
-      };
-      this.#zoomSize = this.bgObj.scaled;
-    } else {
-      this.#dragOffset = {
-        x: 0,
-        y: 0
-      };
-      this.#zoomSize = this.spliceBgList && this.spliceBgList.length ? this.spliceBgList[this.spliceBgList.length-1].scaled : 1;
-    }
+    this.#dragOffset = {
+      x: 0,
+      y: 0
+    };
+    this.#zoomSize = 1;
   }
   // 获取缩放倍数
   getZoomSize() {
@@ -206,27 +298,59 @@ export default class samoBoard {
       default: this.bgObj ? this.bgObj.scaled : 1
     };
   }
-  // 放大
-  zoomIn(step = 0.05) {
-    this.#zoomSize = this.#calcCurrentZoomSize(this.#zoomSize, true, step);
-    if (this.#oldZoomSize !== this.#zoomSize) {
-      this.#calcZoomedDragoffsetDeltaSize();
+  // 检验是否双击
+  detectIsDBClick(ctime) {
+    this.#clickTimeLogs.unshift(ctime);
+    if (this.#clickTimeLogs.length > 2) {
+      this.#clickTimeLogs = this.#clickTimeLogs.slice(0, 2);
+    }
+    if (this.#clickTimeLogs.length !== 2) {
+      return false;
+    }
+    const _deltaTime = Math.abs(this.#clickTimeLogs[0] - this.#clickTimeLogs[1]);
+
+    if (_deltaTime <= 200) {
+      return true;
+    } else {
+      return false;
     }
   }
-  // 缩小
-  zoomOut(step = 0.05) {
-    this.#zoomSize = this.#calcCurrentZoomSize(this.#zoomSize, false, step);
+  // 放大
+  zoomIn({offsetX, offsetY}) {
+    const _offsetX = offsetX === undefined ? Math.floor(this.#canvas.clientWidth/2) : offsetX;
+    const _offsetY = offsetY === undefined ? Math.floor(this.#canvas.clientHeight/2) : offsetY;
+    this.#zoomSize = Number((this.#zoomSize + this.#zoomStep).toFixed(2))
+    this.#zoomSize = Math.min(this.#maxZoomSize, this.#zoomSize)
     if (this.#oldZoomSize !== this.#zoomSize) {
-      this.#calcZoomedDragoffsetDeltaSize(false);
+      this.#dragOffset = {
+        x: this.#dragOffset.x-_offsetX*this.#zoomStep,
+        y: this.#dragOffset.y-_offsetY*this.#zoomStep
+      }
     }
+    this.#oldZoomSize = this.#zoomSize
+  }
+  // 缩小
+  zoomOut({offsetX, offsetY}) {
+    const _offsetX = offsetX === undefined ? Math.floor(this.#canvas.clientWidth/2) : offsetX;
+    const _offsetY = offsetY === undefined ? Math.floor(this.#canvas.clientHeight/2) : offsetY;
+    this.#zoomSize = Number((this.#zoomSize - this.#zoomStep).toFixed(2))
+    this.#zoomSize = Math.max(this.#minZoomSize, this.#zoomSize)
+    if (this.#oldZoomSize !== this.#zoomSize) {
+      this.#dragOffset = {
+        x: this.#dragOffset.x+_offsetX*this.#zoomStep,
+        y: this.#dragOffset.y+_offsetY*this.#zoomStep
+      }
+    }
+    this.#oldZoomSize = this.#zoomSize
   }
   // 设置画图类型
   setDrawType(type='pointer', options={}) {
     this.#drawType = type;
+    // console.log(this.#drawType)
     this.#canvas.removeEventListener('mousedown', this.#pencilDownFn, false);
-    this.#canvas.removeEventListener('mousedown', this.#pencilMoveFn, false);
-    this.#canvas.removeEventListener('mousedown', this.#pencilUpFn, false);
-    this.#canvas.removeEventListener('mousedown', this.#pencilOutFn, false);
+    this.#canvas.removeEventListener('mousemove', this.#pencilMoveFn, false);
+    this.#canvas.removeEventListener('mouseup', this.#pencilUpFn, false);
+    this.#canvas.removeEventListener('mouseout', this.#pencilOutFn, false);
     if (this[`${this.#drawType}DownFn`]) {
       this.#pencilDownFn = e => this[`${this.#drawType}DownFn`](e, options);
       this.#canvas.addEventListener('mousedown', this.#pencilDownFn, false);
@@ -236,7 +360,7 @@ export default class samoBoard {
       this.#canvas.addEventListener('mousemove', this.#pencilMoveFn, false);
     }
     if (this[`${this.#drawType}UpFn`]) {
-      this.#pencilMoveFn = e => this[`${this.#drawType}UpFn`](e, options);
+      this.#pencilUpFn = e => this[`${this.#drawType}UpFn`](e, options);
       this.#canvas.addEventListener('mouseup', this.#pencilUpFn, false);
     }
     if (this[`${this.#drawType}OutFn`]) {
@@ -251,43 +375,86 @@ export default class samoBoard {
       strokeStyle: obj.strokeStyle,
       fillStyle: obj.fillStyle,
     })
-    let _path2d = new Path2D();
-    
     switch(obj.type) {
       case "rect":
+        let _path2d = new Path2D();
         _path2d.rect(obj.x, obj.y, obj.width, obj.height);
-        ctx.storke()
-        ctx.fill()
+        ctx.stroke(_path2d)
+        ctx.fill(_path2d)
         break;
     }
   }
+  pointerDownFn(e) {
+    if (e.button === 0) {
+      this.#hoverPoint = {
+        x: e.offsetX,
+        y: e.offsetY,
+      }
+      // console.log(this.#hoverPoint)
+    }
+  }
+  pointerMoveFn(e) {
+  }
+  pointerUpFn(e) {
+  }
+  pointerOutFn(e) {
+    
+  }
   #renderDraws() {
+    if (this.#draws && this.#draws.length) {
+      this.#draws.forEach((val,index) => {
+        this.#renderDarw(this.#ctx, val)
+      })
+    }
+    
     // const _ctxStyle = this.constructor.mergeDeep(this.opts.ctxStyle, {
     //   strokeStyle: 'gray'
     // })
     // this.#setCtx(this.#ctx, _ctxStyle)
-    this.#ctx.translate(100, 100);
-    this.#ctx.stroke(this.renderCursor('location'))
+    // this.#ctx.translate(100, 100);
+    // this.#ctx.stroke(this.renderCursor('location'))
   }
   #renderTmpDraw() {
     this.#renderDarw(this.#ctx, this.#tmpDraw)
+  }
+  // 滚动缩放
+  #canvasWheel(e) {
+    const _wheelDelta = e.wheelDelta;
+    if (e.ctrlKey && Math.abs(_wheelDelta) > 0) {
+      if (_wheelDelta > 0) {
+        
+        this.zoomIn({
+          offsetX:e.offsetX, 
+          offsetY:e.offsetY
+        });
+      } else {
+        this.zoomOut({
+          offsetX:e.offsetX, 
+          offsetY:e.offsetY
+        });
+      }
+      e.preventDefault();
+      e.stopPropagation();
+    }
   }
   renderCursor(cursorName) {
     return new Path2D(this.#svgCursors[cursorName].path)
   }
   setDrawsData(draws) {
-    this.#draws = draws
+    console.log('set draws data')
+    this.#draws = this.constructor.cloneDeep(draws)
   }
   setCustomDrawType({type, downFn, moveFn, upFn, outFn}) {
     this.#drawType = type;
     this.#canvas.removeEventListener('mousedown', this.#pencilDownFn, false);
-    this.#canvas.removeEventListener('mousedown', this.#pencilMoveFn, false);
-    this.#canvas.removeEventListener('mousedown', this.#pencilUpFn, false);
-    this.#canvas.removeEventListener('mousedown', this.#pencilOutFn, false);
+    this.#canvas.removeEventListener('mousemove', this.#pencilMoveFn, false);
+    this.#canvas.removeEventListener('mouseup', this.#pencilUpFn, false);
+    this.#canvas.removeEventListener('mouseout', this.#pencilOutFn, false);
     let _params = {
-      draws: cloneDeep(this.#draws),
+      draws: this.#draws,
       tmpDraw: this.#tmpDraw,
-      drawPrototype: cloneDeep(this.#drawPrototype)
+      spccebarPressed: this.#spacebarPressed,
+      drawPrototype: this.#drawPrototype
     }
     if (downFn) {
       this.#pencilDownFn = (e) => downFn(e, _params)
@@ -306,7 +473,13 @@ export default class samoBoard {
       this.#canvas.addEventListener('mouseout', this.#pencilOutFn, false);
     }
   }
+  #renderCustomAction() {
+    if (this.customRender){
+      this.customRender({ctx:this.#ctx, draws: this.#draws})
+    }
+  }
   renderBoard() {
+    this.#ctx.clearRect(0, 0, this.#wrap.clientWidth, this.#wrap.clientHeight)
     this.#ctx.setTransform(1, 0, 0, 1, 0, 0);
     this.#ctx.scale(this.#zoomSize, this.#zoomSize);
     this.#ctx.translate(this.#dragOffset.x / this.#zoomSize, this.#dragOffset.y / this.#zoomSize);
@@ -314,6 +487,7 @@ export default class samoBoard {
     this.#ctx.globalCompositeOperation = 'source-over';
     this.#renderDraws()
     this.#renderTmpDraw()
+    this.#renderCustomAction()
     window.requestAnimationFrame(() => this.renderBoard());
   }
 }
